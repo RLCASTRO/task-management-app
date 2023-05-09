1d16199cebd279ab2fce1766745a44ef2c1b01e1
Rodrigo Castro <castro.rl@gmail.com>
2023-05-09 14:35:08 -0300

    Created the createTask() to handle post requests with data in the body to create new tasks. created the getTaskById to get one single task passing a path parameter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks') //this is the route localhost:3000/tasks
 export class TasksController {
   //This will inject tasksService into this controller.
   constructor(private tasksService: TasksService) {} //the private automatically sets the taskService as a property of the class with a type of TasksService
 
-  // The controller method to receive GET requests on the tasks endpoint
+  // The TasksController method to receive GET requests on the tasks endpoint
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks(); //this will call the getAllTasks method from the injected service tasks.service
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id); //this will call the getAllTasks method from the injected service tasks.service
+  }
+
+  //This controller method will handle POST requests on the tasks endpoint
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 
   //You can have methods here
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = []; //only this service has access to this private property
-
+  private task: Task;
   //the controller needs to talk to the service
   //this will be public by default if you dont define the visibility modifier.
   public getAllTasks(): Task[] {
     return this.tasks;
   }
 
-  public createTask(title: string, description): Task {
+  public getTaskById(taskId: string): Task {
+    //filter the task by id
+    this.task = this.tasks.find((task) => task.id === taskId);
+
+    return this.task;
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     //creates the task object
     const task: Task = {
       id: uuid(),
