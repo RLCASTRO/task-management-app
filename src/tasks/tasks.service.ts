49ec9f6745f4e57084427debf64d3a354f955c4e
Rodrigo Castro <castro.rl@gmail.com>
2023-05-10 09:26:20 -0300

    Updating the deleteTaskById function to match the course solution

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,13 +19,8 @@ export class TasksService {
 
   //this will return an array of tasks that does not have the id passed as argument
   public deleteTaskById(taskId: string): string {
-    const index = this.tasks.findIndex((task) => task.id === taskId);
-    if (index === -1) {
-      return 'Task not found';
-    } else {
-      this.tasks.splice(index, 1);
-      return `Task number deleted`;
-    }
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    return `Task number deleted`;
   }
 
   public createTask(createTaskDto: CreateTaskDto): Task {
