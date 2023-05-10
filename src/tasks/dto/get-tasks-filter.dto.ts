e18a3ec21ec757eb4399ad3a9c05a219196a5908
Rodrigo Castro <castro.rl@gmail.com>
2023-05-10 16:12:03 -0300

    Added validation to the getTasksWithSearch() method by editing the filterDto file adding validation decorators to each property

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,11 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 export class GetTasksFilterDto {
   //The ? means both fields are optional
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+  @IsOptional()
+  @IsString()
   search?: string;
 }
